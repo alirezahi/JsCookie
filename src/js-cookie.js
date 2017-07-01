98fb23897eca2856474dddc4a936374adc7b4753
Alireza <alireza97hi@gmail.com>
2017-07-01 18:07:40 +0430

    deleteCookies function added

--- src/js-cookie.js
+++ src/js-cookie.js
@@ -7,3 +7,9 @@ function allCookies() {
     })
     return resultCookie;
 }
+
+
+function deleteCookie(name,path){
+  // In order to delete a cookie Just specify its name and if you need to reomve it from specific path you can do that too by passing path variable
+  document.cookie = name + "=" + "; expires=Thu, 01 Jan 1970 00:00:01 GMT" + ((path) ? "; path=" + path : "") + ";";
+}
